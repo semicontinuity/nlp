ee2f0dd36c8e63f99de30fe069ddfe506ab76512
semicontinuity <igor.a.karpov@gmail.com>
2020-05-20 16:30:30 +0300

    tqdm for notebook

--- lopuhin_transformer_lm/lm/main.py
+++ lopuhin_transformer_lm/lm/main.py
@@ -1,8 +1,8 @@
 def isnotebook():
     try:
         shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
+        if shell == 'ZMQInteractiveShell' or 'Shell':
+            return True   # Jupyter notebook or qtconsole or Colab
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
